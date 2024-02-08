3ae1b4aff5eeb6e1301cd75ad9c6286434bee948
gabriel <gabriel@zekini.com>
2024-02-08 23:12:33 +0200

    WIP docker setup

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from datetime import date
 from typing import TYPE_CHECKING
 from uuid import UUID
 
-from sqlalchemy import ForeignKey, select
+from sqlalchemy import Float, ForeignKey, Text, Uuid, select
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from litestar import Litestar, get
@@ -15,19 +15,31 @@ if TYPE_CHECKING:
 
 # the SQLAlchemy base includes a declarative model for you to use in your models.
 # The `Base` class includes a `UUID` based primary key (`id`)
-class Author(UUIDBase):
-    name: Mapped[str]
-    dob: Mapped[date]
-    books: Mapped[list["Book"]] = relationship(back_populates="author", lazy="selectin")
+# class Product(UUIDBase):
+#     name: Mapped[str]
+#     price: Mapped[date]
+    # books: Mapped[list["Book"]] = relationship(back_populates="author", lazy="selectin")
 
 
 # The `AuditBase` class includes the same UUID` based primary key (`id`) and 2
 # additional columns: `created_at` and `updated_at`. `created_at` is a timestamp of when the
 # record created, and `updated_at` is the last time the record was modified.
-class Book(UUIDAuditBase):
-    title: Mapped[str]
-    author_id: Mapped[UUID] = mapped_column(ForeignKey("author.id"))
-    author: Mapped[Author] = relationship(lazy="joined", innerjoin=True, viewonly=True)
+class Product(UUIDAuditBase):
+    title: Mapped[str] = mapped_column(Text(), nullable=False)
+    active: Mapped[bool] = mapped_column()
+    description: Mapped[str] = mapped_column(Text(), nullable=False)
+    image: Mapped[str] = mapped_column(Text(), nullable=False)
+    product_category_id: Mapped[UUID] = mapped_column(Uuid(),ForeignKey("product_category.id"), nullable=True)
+    price:  Mapped[float] = mapped_column(Float(), nullable=False)
+    prep_time: Mapped[str] = mapped_column(Text(), nullable=False)
+    # size_pricing: Mapped[Author] = relationship(lazy="joined", innerjoin=True, viewonly=True)
+
+
+class ProductCategory(UUIDAuditBase):
+    name: Mapped[str]
+    active: Mapped[bool] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    # size_pricing: Mapped[Author] = relationship(lazy="joined", innerjoin=True, viewonly=True)
 
 
 session_config = AsyncSessionConfig(expire_on_commit=False)
@@ -43,10 +55,10 @@ async def on_startup() -> None:
         await conn.run_sync(UUIDBase.metadata.create_all)
 
 
-@get(path="/authors")
-async def get_authors(db_session: "AsyncSession", db_engine: "AsyncEngine") -> list[Author]:
+@get(path="/products")
+async def get_authors(db_session: "AsyncSession", db_engine: "AsyncEngine") -> list[Product]:
     """Interact with SQLAlchemy engine and session."""
-    return list(await db_session.scalars(select(Author)))
+    return list(await db_session.scalars(select(Product)))
 
 
 app = Litestar(
