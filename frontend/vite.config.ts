5e0e2bdc4c00118d6acd17611bdd5b601d6c1bcf
gabriel <gabriel@zekini.com>
2024-06-02 12:34:27 +0200

    FE stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	server: {
+		allow: ['/home/gabriel/smart-dash/frontend/assets/'], // Add the directory path
+	  },
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+		  $assets: path.resolve(__dirname, 'assets/'),
+		//   $lib: path.resolve(__dirname, 'src/lib/'),
+		//   $app: path.resolve(__dirname, 'src/mocks/app')
+	
+		},
+	  }
 });
